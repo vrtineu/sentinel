99fa39e485bda57e069264b7577a971929cf5bcb
vrtineu <vininro@gmail.com>
2024-10-02 22:50:37 -0300

    chore: add absinthe library to the project

--- mix.exs
+++ mix.exs
@@ -44,7 +44,9 @@ defmodule Sentinel.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:absinthe, "~> 1.7"},
+      {:absinthe_plug, "~> 1.5"}
     ]
   end
 
